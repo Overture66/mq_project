41f24d6a403a6739907084836b133e3a467079d5
Overture <chaoccc2022@163.com>
2024-03-06 12:32:04 +0800

    简单模式_消费者

--- mq_demo/src/main/com/sc/rbbitmq/simple/Producer.java
+++ mq_demo/src/main/com/sc/rbbitmq/simple/Producer.java
@@ -1,2 +1,38 @@
-package com.sc.rbbitmq.simple;public class Producer {
+package com.sc.rbbitmq.simple;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.sc.rbbitmq.utlis.ConnectionUtlis;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Producer {
+    //queue的名字
+    static final String queueName = "simple_queue";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+
+        Connection connection = ConnectionUtlis.getConnection();
+        //声明队列
+        Channel channel = connection.createChannel();
+        //参数：1队列名 2.是否持久化 3.是否独占连接 4.是否不用时自动删除 5.队列其他参数
+        channel.queueDeclare(queueName, true, false, false, null);
+
+        String message = "hello rabbitmq...";
+
+        /*参数：交换机名字，默认使用defalut exchange
+         2.routingkey，在简单模式可以写queuename
+         3.消息的其他属性
+         4.消息数据 */
+        channel.basicPublish("", queueName, null, message.getBytes());
+
+        System.out.println("send message: " + message);
+
+        //释放资源
+        channel.close();
+        connection.close();
+
+    }
 }
---
+++ mq_demo/src/main/com/sc/rbbitmq/utlis/ConnectionUtlis.java
@@ -0,0 +1,25 @@
+package com.sc.rbbitmq.utlis;
+
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class ConnectionUtlis {
+
+    public static Connection getConnection() throws IOException, TimeoutException, IOException, TimeoutException {
+        //创建连接工厂
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("192.168.31.128");
+        factory.setPort(5672);
+        factory.setVirtualHost("/sc");
+        factory.setUsername("sc");
+        factory.setPassword("sc");
+
+
+        //创建连接
+        Connection connection = factory.newConnection();
+        return connection;
+    }
+}
